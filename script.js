6371c7ec4f51c2d7ce3e4105ab523edcf0dd4e47
Juanscn <juan.camelo@edu.sait.ca>
2024-11-23 21:50:22 -0700

    Buttons
    Started button funcionality

---
+++ script.js
@@ -0,0 +1,42 @@
+function defaultButtonColours () {
+    document.getElementById('buttonSingularity').style.backgroundColor = 'black';
+    document.getElementById('buttonInflation').style.backgroundColor = 'black';
+    document.getElementById('buttonParticles').style.backgroundColor = 'black';
+    document.getElementById('buttonAtoms').style.backgroundColor = 'black';
+    document.getElementById('buttonStars').style.backgroundColor = 'black';
+}
+
+buttonSingularity.addEventListener('click', function(){
+    defaultButtonColours();
+    document.getElementById('image').src = 'images/singularity.png';
+    document.getElementById('buttonSingularity').style.backgroundColor = '#b3b2b3';
+    document.getElementById('buttonSingularity').style.color = '#000000';
+})
+
+buttonSingularity.addEventListener('click', function(){
+    defaultButtonColours();
+    document.getElementById('image').src = 'images/singularity.png';
+    document.getElementById('buttonSingularity').style.backgroundColor = '#b3b2b3';
+    document.getElementById('buttonSingularity').style.color = '#000000';
+})
+
+buttonSingularity.addEventListener('click', function(){
+    defaultButtonColours();
+    document.getElementById('image').src = 'images/singularity.png';
+    document.getElementById('buttonSingularity').style.backgroundColor = '#b3b2b3';
+    document.getElementById('buttonSingularity').style.color = '#000000';
+})
+
+buttonSingularity.addEventListener('click', function(){
+    defaultButtonColours();
+    document.getElementById('image').src = 'images/singularity.png';
+    document.getElementById('buttonSingularity').style.backgroundColor = '#b3b2b3';
+    document.getElementById('buttonSingularity').style.color = '#000000';
+})
+
+buttonSingularity.addEventListener('click', function(){
+    defaultButtonColours();
+    document.getElementById('image').src = 'images/singularity.png';
+    document.getElementById('buttonSingularity').style.backgroundColor = '#b3b2b3';
+    document.getElementById('buttonSingularity').style.color = '#000000';
+})
